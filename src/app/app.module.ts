6e080f1ac29bfa55f184a7741c0050181b6723d4
Sebastián Guevara <rlxsebas@gmail.com>
2018-11-12 21:34:35 -0500

    feat: Basic structure Angular Element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { UserPollComponent } from './user-poll/user-poll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    UserPollComponent
+  ],
+  entryComponents: [
+    UserPollComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(UserPollComponent, { injector: this.injector });
+    customElements.define('user-poll', el);
+  }
+
+  ngDoBootstrap() {
+  }
+}
