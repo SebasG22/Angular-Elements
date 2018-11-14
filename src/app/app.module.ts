96b8df92ac7f039f27a0fbaf7128c810c9bb3dfa
Sebastián Guevara <rlxsebas@gmail.com>
2018-11-13 22:41:34 -0500

    feat: WIP Angular Elemtns with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { UserPollComponent } from './user-poll/user-poll.component';
 
+const config = {
+  apiKey: 'AIzaSyBHSKHd7QaeS_F3IYFMvNvGeWgrwUHq-F4',
+  authDomain: 'myapp-3087e.firebaseapp.com',
+  databaseURL: 'https://myapp-3087e.firebaseio.com',
+  projectId: 'myapp-3087e',
+  storageBucket: 'myapp-3087e.appspot.com',
+  messagingSenderId: '284572120270'
+};
 @NgModule({
   declarations: [
     UserPollComponent
@@ -13,7 +23,9 @@ import { UserPollComponent } from './user-poll/user-poll.component';
     UserPollComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
   ],
   providers: [],
 })
--- src/app/user-poll/user-poll.component.ts
+++ src/app/user-poll/user-poll.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AngularFirestoreDocument, AngularFirestore } from '@angular/fire/firestore';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-poll',
   templateUrl: './user-poll.component.html',
-  styleUrls: ['./user-poll.component.scss']
+  styleUrls: ['./user-poll.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class UserPollComponent implements OnInit {
 
-  constructor() { }
+  public yes: number;
+  public no: number;
+  public hasVoted = false;
+  pollRef: AngularFirestoreDocument<any>;
+
+  constructor(private afs: AngularFirestore) {
+    this.afs.firestore.settings({ timestampsInSnapshots: true });
+  }
 
   ngOnInit() {
+    this.pollRef = this.afs.doc('polls/elements');
+
+    this.pollRef.valueChanges().pipe(
+      tap((doc) => {
+        if (doc) {
+          this.yes = (doc.yes) ? doc.yes : 0;
+          this.no = (doc.no) ? doc.no : 0;
+        } else {
+          this.yes = 0;
+          this.no = 0;
+        }
+      })
+    ).subscribe();
+  }
+
+  public vote(val: string) {
+    this.hasVoted = true;
+    this.pollRef.update({ [val]: this[val] + 1 });
+  }
+
+  get yesPercent() {
+    return (this.yes / (this.yes + this.no)) * 100;
+  }
+
+  get noPercent() {
+    return (this.no / (this.yes + this.no)) * 100;
   }
 
 }
